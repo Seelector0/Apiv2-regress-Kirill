7352630ce0879b3fed03fbbe5c4e4974b88e8b84
KirilOdintsov <kirill.odintsov@pimpay.ru>
2023-05-26 19:43:41 +0300

    add clean db

--- environment.py
+++ environment.py
@@ -7,7 +7,6 @@ load_dotenv()
 
 
 class Env:
-
     if os.environ.get('STAGE') == 'Local':
         URL = os.environ.get('URL_LOCAL')
         client_id = os.environ.get('CLIENT_ID_LOCAL')
@@ -15,10 +14,13 @@ class Env:
         db_user = os.environ.get('DB_USER_LOCAL')
         db_password = os.environ.get('DB_PASSWORD_LOCAL')
         db_host = os.environ.get('DB_HOST_LOCAL')
+        db_user_id = os.environ.get('USER_ID_LOCAl')
     elif os.environ.get('STAGE') == 'Develop':
-        URL = os.environ.get('URL')
-        client_id = os.environ.get('CLIENT_ID')
-        client_secret = os.environ.get('CLIENT_SECRET')
+        URL = os.environ.get('URL_DEV')
+        client_id = os.environ.get('CLIENT_ID_DEV')
+        client_secret = os.environ.get('CLIENT_SECRET_DEV')
         db_user = os.environ.get('DB_USER_DEV')
         db_password = os.environ.get('DB_PASSWORD_DEV')
         db_host = os.environ.get('DB_HOST_DEV')
+    else:
+        raise ValueError(f"некорректное значение для параметра STAGE: {os.environ.get('STAGE')}")
