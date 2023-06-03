df7744da89382b5459dd2d7fe485e6713ebb0749
Kirill Odintsov <110476859+Seelector0@users.noreply.github.com>
2023-06-03 23:58:38 +0300

    Update clear_db.py

--- utils/clear_db.py
+++ utils/clear_db.py
@@ -26,11 +26,34 @@ def clear_db():
         count = cursor.rowcount
         print(count, "Запись о складах успешно удалена")
 
-        # Выполнение SQL-запроса для удаления закаов
+        # Выполнение SQL-запроса для удаления заказов
         cursor.execute(f"DELETE FROM \"order\".\"order\" WHERE user_id = '{Env.db_user_id}'")
         connection.commit()
         count = cursor.rowcount
         print(count, "Запись о заказах успешно удалена")
+
+        # Выполнение SQL-запроса для удаления связанных записей в таблице "order.order_parcel"
+        cursor.execute("""
+                    DELETE FROM "order".order_parcel 
+                    WHERE parcel_id IN (
+                        SELECT id FROM "order".parcel WHERE user_id = %s
+                    )
+                """, (Env.db_user_id,))
+        count_order_parcel = cursor.rowcount
+        print(count_order_parcel, "Записи в таблице order.order_parcel успешно удалены")
+        connection.commit()
+
+        # Выполнение SQL-запроса для удаления партий
+        cursor.execute(f"DELETE FROM \"order\".parcel WHERE user_id = '{Env.db_user_id}'")
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Запись о партиях успешно удалена")
+
+        # Выполнение SQL-запроса для удаления конекшенов
+        cursor.execute(f"DELETE FROM customer.credential WHERE user_id = '{Env.db_user_id}'")
+        connection.commit()
+        count = cursor.rowcount
+        print(count, "Запись о конекшинах успешно удалена")
 
     except (Exception, Error) as error:
         print("Ошибка при работе с PostgreSQL", error)
@@ -39,6 +62,3 @@ def clear_db():
             cursor.close()
             connection.close()
             print("Соединение с PostgreSQL закрыто")
-
-
-
