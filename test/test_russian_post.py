4b9d59f08743390d4bd779b370197a6f34e9b28b
KirilOdintsov <kirill.odintsov@pimpay.ru>
2023-06-04 12:40:42 +0300

    add x-trace-id

--- test/test_russian_post.py
+++ test/test_russian_post.py
@@ -10,8 +10,9 @@ import pytest
 class TestDeliveryService:
 
     @allure.title("Создание подключения POST")
-    def test_post_connection(self, access_token, connection, shop):
-        result_post_connections = connection[0]
+    def test_post_connection(self, access_token, shop):
+        result_post_connections = DeliveryServiceApi.delivery_service_russian_post(shop_id=shop[1],
+                                                                                   headers=access_token)
         Checking.check_status_code(result=result_post_connections, status_code=201)
         Checking.check_json_required_keys(result=result_post_connections, required_key=['id', 'type', 'url', 'status'])
         Checking.check_json_value(result=result_post_connections, key_name='type', expected_value='Delivery')
@@ -20,8 +21,7 @@ class TestDeliveryService:
                                                    check_value=shop[1],
                                                    regexp_pattern=r'\/shops\/(.+)\/delivery_services\/.*')
 
-    @allure.title("Получение настроек службы доставки GET")
-    def test_get_connection(self, access_token, shop):
+        """Получение настроек службы доставки GET"""
         result_get_connections = DeliveryServiceApi.get_delivery_service_code(shop_id=shop[1], headers=access_token,
                                                                               code='RussianPost')
         Checking.check_status_code(result=result_get_connections, status_code=200)
@@ -33,8 +33,7 @@ class TestDeliveryService:
         Checking.check_json_value_nested(result=result_get_connections, key_tuple=('credentials', 'data', 'type'),
                                          expected_value="integration")
 
-    @allure.title("Получение настроек всех служб доставки GET")
-    def test_get_all_connection(self, access_token, shop):
+        """Получение настроек всех служб доставки GET"""
         result_get_all_connections = DeliveryServiceApi.get_delivery_service(shop_id=shop[1], headers=access_token)
         Checking.check_status_code(result=result_get_all_connections, status_code=200)
         Checking.check_json_required_keys_array(result=result_get_all_connections, required_key=['code', 'name',
@@ -42,8 +41,7 @@ class TestDeliveryService:
                                                                                                  'active', 'visibility',
                                                                                                  'type', 'moderation'])
 
-    @allure.title("Редактирование полей службы доставки PATCH")
-    def test_patch_connection(self, access_token, shop):
+        """Редактирование полей службы доставки PATCH"""
         result_patch_connections = DeliveryServiceApi.patch_delivery_service_code(shop_id=shop[1], code='RussianPost',
                                                                                   headers=access_token)
         Checking.check_status_code(result=result_patch_connections, status_code=200)
@@ -62,27 +60,23 @@ class TestDeliveryService:
                                          key_tuple=('credentials', 'settings', 'tariffs', 'exclude'),
                                          expected_value=['14', '25'])
 
-    @allure.title("Обновление службы доставки PUT")
-    def test_put_connection(self, access_token, shop):
+        """Обновление службы доставки PUT"""
         result_put_connections = DeliveryServiceApi.put_delivery_service(shop_id=shop[1], code='RussianPost',
                                                                          headers=access_token)
         Checking.check_status_code(result=result_put_connections, status_code=409)
 
-    @allure.title("Удаление службы доставки DELETE")
-    def test_delete_connection(self, access_token, shop):
+        """Удаление службы доставки DELETE"""
         result_delete_connections = DeliveryServiceApi.delete_delivery_service(shop_id=shop[1], code='RussianPost',
                                                                                headers=access_token)
         Checking.check_status_code(result=result_delete_connections, status_code=409)
 
-    @allure.title("Деактивация службы доставки POST")
-    def test_deactivate_connection(self, access_token, shop):
+        """Деактивация службы доставки POST"""
         result_deactivate_connections = DeliveryServiceApi.delivery_service_deactivate(shop_id=shop[1],
                                                                                        code='RussianPost',
                                                                                        headers=access_token)
         Checking.check_status_code(result=result_deactivate_connections, status_code=204)
 
-    @allure.title("Активация службы доставки POST")
-    def test_activate_connection(self, access_token, shop):
+        """Активация службы доставки POST"""
         result_activate_connections = DeliveryServiceApi.delivery_service_activate(shop_id=shop[1], code='RussianPost',
                                                                                    headers=access_token)
         Checking.check_status_code(result=result_activate_connections, status_code=204)
@@ -95,30 +89,28 @@ class TestOrder:
     def test_post_order(self, access_token, shop, warehouse, payment_type):
         result_post_order = OrderApi.create_order(shop_id=shop[1], warehouse_id=warehouse[1], headers=access_token,
                                                   payment_type=payment_type, delivery_type='PostOffice')
+        order_id = result_post_order.json()["id"]
         # Записываем id заказ в переменную
         if payment_type == "Paid":
             TestOrder.order_id = result_post_order.json()["id"]
-        Checking.check_status_code(result=result_post_order, status_code=202)
+        Checking.check_status_code(result=result_post_order, status_code=201)
         Checking.check_json_required_keys(result=result_post_order, required_key=['id', 'type', 'url', 'status'])
         Checking.check_json_value(result=result_post_order, key_name='type', expected_value='Order')
         Checking.check_json_value(result=result_post_order, key_name='status', expected_value=201)
         Checking.check_json_search_regexp_in_value(result=result_post_order.json().get('url'),
                                                    check_value=result_post_order.json().get('id'),
                                                    regexp_pattern=r'\/orders\/(.+)$')
-
-    def test_get_all_orders(self, access_token):
+        """get all"""
         result_get_all_orders = OrderApi.get_orders_all(headers=access_token)
         Checking.check_status_code(result=result_get_all_orders, status_code=200)
         Checking.check_unique_ids(result=result_get_all_orders, id_key='id')
         Checking.check_json_required_keys_array(result=result_get_all_orders,
                                                 required_key=['id', 'number', 'addressTo', 'data', 'parcel', 'status',
                                                               'statusReason', 'state', 'stateMessage', 'created'])
-
-    def test_get_order(self, access_token):
-        order_id = TestOrder.order_id
+        """get"""
         Checking.checking_state_order(order_id=order_id, headers=access_token)
         result_get_order = OrderApi.get_orders(order_id=order_id, headers=access_token, x_trace_id="Get_order_test")
-        Checking.check_status_code(result=result_get_order, status_code=2002)
+        Checking.check_status_code(result=result_get_order, status_code=200)
         Checking.check_json_required_keys(result=result_get_order, required_key=['id', 'number', 'addressTo', 'data',
                                                                                  'parcel', 'status', 'statusReason',
                                                                                  'state', 'stateMessage', 'created'])
